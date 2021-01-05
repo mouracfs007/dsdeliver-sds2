71ec1a325041e31c3f9f6be28e83137dcea703da
Daniel <danielmourajuve10@gmail.com>
2021-01-05 19:56:04 -0300

    PUT / orders /{id}/delivered

--- dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ dsdeliver/backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -46,6 +46,17 @@ public class OrderService {
 		return new OrderDTO(order);
 	}
 	
+	@Transactional
+	public OrderDTO setDelivered(Long id) {
+		Order order = repository.getOne(id);
+		order.setStatus(OrderStatus.DELIVERED);
+		order =repository.save(order);
+		return new OrderDTO(order);
+	}
+	
+
 }
+	
+
 	
 
