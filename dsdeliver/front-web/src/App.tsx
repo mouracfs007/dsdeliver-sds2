3bf243ef24f1e55b2c167de9333837e461963296
Daniel <danielmourajuve10@gmail.com>
2021-01-09 00:44:28 -0300

    Add routes and StepsHeader

--- dsdeliver/front-web/src/App.tsx
+++ dsdeliver/front-web/src/App.tsx
@@ -1,13 +1,9 @@
 import './App.css';
-import Home from './Home';
-import Navbar from './Navbar';
+import Routes from './Routes';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Home />
-    </div>
+    <Routes />
   );
 }
 
