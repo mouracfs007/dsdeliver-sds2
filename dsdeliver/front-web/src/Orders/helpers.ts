3d7ebca15b0e1c70bbf56f0ea29e42e2c970836c
Daniel <danielmourajuve10@gmail.com>
2021-01-10 12:32:50 -0300

    Finish front-web

---
+++ dsdeliver/front-web/src/Orders/helpers.ts
@@ -0,0 +1,17 @@
+import { Product }  from './types';
+
+export function CheckIsSelected (selectedProducts: Product[], product: Product) {
+    return selectedProducts.some(item => item.id === product.id);
+}
+
+
+export function formatPrice(price: number) {
+    const formatter = new Intl.NumberFormat('pt-BR', {
+        style:'currency',
+        currency: 'BRL',
+        minimumFractionDigits:2
+    });
+
+    return formatter.format(price);
+
+}
